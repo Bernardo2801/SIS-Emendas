1011a9af3a2c93fae6a939e5242157825437fc88
unknown <bernardoaraujoalves@gmail.com>
2024-05-08 22:35:08 -0300

    looker

--- resources/views/lookers/cidade-beneficiario.blade.php
+++ resources/views/lookers/cidade-beneficiario.blade.php
@@ -19,8 +19,8 @@
     <section class="p-2" data-aos="fade-down">
         <div class="mt-2 px-8">
             <div class="flex flex-col sm:flex-row justify-center items-center">
-                <div class="w-full h-full sm:w-full sm:h-full md:w-full md:h-full lg:w-full lg:h-full xl:w-full xl:h-full 2xl:w-[1230px] 2xl:h-full p-6 bg-gray-200 rounded-lg shadow-lg hover:shadow-2xl mb-4 mr-2 flex flex-col sm:flex-row justify-center items-center justify-center items-center">
-                    <iframe src="https://lookerstudio.google.com/embed/reporting/0de10e8f-6251-4099-b173-9ca89a9df191/page/DBreD" frameborder="0" style="border:0" allowfullscreen sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox" class="w-full h-[700px] sm:w-full sm:h-[670px] md:w-full md:h-[700px] lg:w-full lg:h-[720px] xl:w-full xl:h-[730px] 2xl:w-full 2xl:h-[730px]">></iframe>
+                <div class="w-full h-full sm:w-full sm:h-full md:w-full md:h-full lg:w-full lg:h-full xl:w-full xl:h-full 2xl:w-[1230px] 2xl:h-full p-6 bg-gray-800 rounded-lg shadow-lg hover:shadow-2xl mb-4 mr-2 flex flex-col sm:flex-row justify-center items-center justify-center items-center">
+                    <iframe src="https://lookerstudio.google.com/embed/reporting/0de10e8f-6251-4099-b173-9ca89a9df191/page/DBreD" frameborder="0" style="border:0" allowfullscreen sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox" class="w-full h-[700px] sm:w-full sm:h-[670px] md:w-full md:h-[700px] lg:w-full lg:h-[720px] xl:w-full xl:h-[730px] 2xl:w-full 2xl:h-[730px]"></iframe>
                 </div>
             </div>
         </div>
