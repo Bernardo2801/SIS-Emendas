2b97f8d39b1452c3a918d23c014892463bb00f69
Bernardo Araujo Alves <bernardo.alves@NOVACAP.SEDE>
2024-05-14 15:51:08 -0300

    login

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -7,6 +7,9 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
+        <!-- Tailwind -->
+        @vite('resources/css/app.css')
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
@@ -21,6 +24,7 @@
         @livewireStyles
     </head>
     <body>
+
         <div class="font-sans text-gray-900 antialiased">
             {{ $slot }}
         </div>
