89ad4adfff814f1f44ccd2232831334457247c5c
Bernardo Araujo Alves <bernardo.alves@NOVACAP.SEDE>
2024-05-15 16:16:32 -0300

    login page

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) !!}>
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'p-2 border border-gray-300 focus:border-[#209D5C] focus:ring-[#209D5C] rounded-xl shadow-sm w-full']) !!}>
